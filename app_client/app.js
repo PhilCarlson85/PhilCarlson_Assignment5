512e338e522e896cd8376f0433c80877c4ff5bd0
PhilCarlson85 <philcarlson85@gmail.com>
2016-11-27 12:29:41 -0600

    Add files via upload

--- app_client/app.js
+++ app_client/app.js
@@ -3,8 +3,8 @@ angular.module('bidderApp', ['ngRoute']);
 function config ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'home/home.view.html'
-            controller: 'homeCtrl'
+            templateUrl: 'home/home.view.html',
+            controller: 'homeCtrl',
             controllerAs: 'vm'
         })
         .otherwise({redirectTo: '/'});
